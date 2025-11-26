a30d767b8c88867fbf0ee278716d0f3ca06a0ffb
Sumit Kumar Shaw <shawsumit6286@gmail.com>
2025-11-26 22:30:07 +0530

    Add setup instructions for React
    Added instructions for creating a React app using Vite.

--- 01-basics/exercise-01.jsx
+++ 01-basics/exercise-01.jsx
@@ -14,3 +14,30 @@ function WelcomeMessage() {
 }
 export default WelcomeMessage;
 
+/* 
+CREATING A REACT APP 
+// npm create vite@latest my-react-app -- --template react
+// cd my-react-app
+// npm install
+// npm run dev
+
+1) npm create vite@latest my-react-app -- --template react
+
+Creates a new folder called my-react-app and scaffolds a React project using Vite’s latest template.
+The -- passes --template react directly to Vite so it knows which starter files to generate. ( index app css main app jsx)
+
+2) cd my-react-app
+
+Moves your terminal into the new project directory.
+All further commands (install, dev) must run inside this folder.
+
+3) npm install
+
+Reads your package.json and downloads all dependencies into node_modules.
+Also generates/updates package-lock.json to lock exact versions.
+
+4) npm run dev
+
+Runs the dev script, which starts Vite’s super-fast development server.
+It serves your React app with hot reload at http://localhost:5173.
+*/
