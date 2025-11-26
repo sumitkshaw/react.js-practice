322c92d989304f5c8721b1e58d6f716a27443470
Sumit Kumar Shaw <shawsumit6286@gmail.com>
2025-11-26 19:37:47 +0530

    React component
    Created a simple React component that displays a welcome message.

---
+++ 01-basics/exercise-01.jsx
@@ -0,0 +1,16 @@
+/*
+Creating our First react Component 
+React Component is a reusable piece of code , more like a building block which helps breaking down complex UI into smaller more manageable pieces.
+
+MY POV: Components are those "<Footer />" which helps add that footer component to any page desired.
+*/
+
+function WelcomeMessage() {
+  return (
+    Hello React 👋
+    This is my first component.
+
+  );
+}
+export default WelcomeMessage;
+
