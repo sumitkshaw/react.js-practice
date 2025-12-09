66e01e869c8782dee4aa42509637c1fe8a90e500
Sumit Kumar Shaw <shawsumit6286@gmail.com>
2025-12-09 19:03:45 +0530

    use-effect

--- 02-hooks/useEffect.jsx
+++ 02-hooks/useEffect.jsx
@@ -1,3 +1,25 @@
 USE EFFECT
 - It lets you run code after the component renders — like fetching data, listening to events, updating the title, timers, etc.
 - It’s React’s way of handling side effects.
+
+import { useEffect, useState } from "react";
+
+function UseEffectExample() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log("Component rendered or updated");
+  }, [count]); // runs only when count changes
+
+  return (
+    <div>
+      <h2>Count: {count}</h2>
+
+      <button onClick={() => setCount(count + 1)}>Increase</button>
+    </div>
+  );
+}
+
+export default UseEffectExample;
+
+- useEffect here is used to log a message every time the component renders or when the count state changes.
